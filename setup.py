84a7a12b06c4edfd25457b939cf19194534319ed
hondo-s <shunsuke.hondo@gmail.com>
2015-11-22 13:15:44 +0900

    update package info

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="PyFile",
     version="1.0.1",
-    description="Python file object wrapper",
-    author="HONDO Shunsuke",
+    description="More human-friendly file access interface",
+    author="Shunsuke Hondo",
     author_email="shunsuke.hondo@gmail.com",
-    url="http://blog.chyka.tokyo/",
+    url="http://github.com/chyka-dev/PyFile",
     license="MIT",
     keywords=["file"],
     packages=find_packages(),
